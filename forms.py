2392875bc06f4cecf6fe139f8712e003eb19ffa1
CJE5SZH <adiachan@outlook.com>
2019-01-11 14:53:34 +0800

    part2-Forms_User commit url_for

--- forms.py
+++ forms.py
@@ -2,8 +2,11 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo
 
+
+# class inherited from FlaskForm
 class RegistrationForm(FlaskForm):
     username = StringField('Username', 
+                            # limititation for username
                             validators=[DataRequired(), Length(min=2, max=24)])
     email = StringField('Email',
                         validators=[DataRequired(), Email()])
@@ -17,5 +20,5 @@ class LoginForm(FlaskForm):
     email = StringField('Email',
                         validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
-    remember = BooleanField('Remember Me')
+    remember = BooleanField('Remember Me')  # check box
     submit = SubmitField('Login')
