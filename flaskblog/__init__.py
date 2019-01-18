ebd5ec98f635c9e24ddcf2a7bdf51e11fa4f56b6
CJE5SZH <adiachan@outlook.com>
2019-01-18 14:31:59 +0800

    part6-user-authentication

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,10 +1,17 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '785372e6b18b41a905283e1fbf9ca6f1'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'  # if no user log in to the page, go to the account page will redirect to login page
+login_manager.login_message_category = 'info'
 
 from flaskblog import routes
